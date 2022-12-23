d08227235fc50b4584eddb9af917c1ec64d99989
piyo <chick.p.1120@gmail.com>
2022-12-23 17:00:34 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+
+export default defineConfig({
+  site: `https://www.chick-p.work`,
+  base: `/`,
+  markdown: {
+    extendDefaultPlugins: true,
+  },
+  integrations: [mdx(), image()],
+});
