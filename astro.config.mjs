b3490c40ba8651d8f0f957e6040f03ec0ac71516
piyo <chick.p.1120@gmail.com>
2023-12-30 17:05:46 +0900

    chore: remove to depend on @astro/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
 
 export default defineConfig({
   site: `https://www.chick-p.work/`,
   base: `/`,
-  markdown: {
-  },
-  integrations: [mdx(), image()],
+  markdown: {},
+  integrations: [mdx()],
 });
