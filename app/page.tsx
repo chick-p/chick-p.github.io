d84975001918151a9fffc6617b00b0310e08c6bf
piyo <chick.p.1120@gmail.com>
2024-08-22 18:36:53 +0900

    feat: add content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Markdown from "./contents/home.mdx"
 import { Config } from "./config";
 
 export default function Home() {
   return (
     <main className="main">
       <h1 className="title-gradient">{Config.site.title}</h1>
+      <Markdown />
     </main>
   )
 }
